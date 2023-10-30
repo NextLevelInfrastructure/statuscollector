01047b42a7ca636c12b5065899907629020f5371
Daniel Dulitz <dulitz@gmail.com>
2023-10-30 12:43:04 -0400

    add sample queries

--- statuscollector/prometheus.py
+++ statuscollector/prometheus.py
@@ -4,6 +4,27 @@ prometheus.py
 
 The Prometheus API as documented in
 https://github.com/4n4nd/prometheus-api-client-python
+
+## For each non-lab core router reporting data to Prometheus,
+## show its 15-minutely ingressing data rate. We should complain if
+## this number is not equal to the number of non-lab core routers
+## that we have.
+
+sum by (instance)(rate(ifInOctets{cabinet!~"lab-.*",role=~"rtc.*"}[15m]))
+
+sonic_stats_rxpower_dbm{cabinet="$cabinet"}
+
+ifAdminStatus{ifAlias="Cust: foo 21080_road", ifIndex="3", ifName="Port3", instance="cabinet-sws02.owner"}
+   ***** values 1 up, 2 down
+
+ifOperStatus{}
+   ***** values: 1 up, 2 down, others are weird
+
+ifHighSpeed{}
+   ***** values: 1000 or 10000
+
+ifInErrors, ifInOctets, ifInDiscards
+ifOutErrors, ifOutOctets
 """
 
 import logging, prometheus_client
