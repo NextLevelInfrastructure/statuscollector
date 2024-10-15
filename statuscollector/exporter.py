854602858565c436291453052e1a1f0362804ed5
Daniel Dulitz <dulitz@gmail.com>
2024-10-15 09:59:40 -0700

    actually call the method to update metric labels when the model changes. also, remove labels when a primary key is removed from the model

--- statuscollector/exporter.py
+++ statuscollector/exporter.py
@@ -90,12 +90,36 @@ class ModelGauge:
         self.idlabel, self.model, self.selector = idlabel, model, selector
         self.id2labelvalues_map = {}
         self.gauge = prometheus_client.Gauge(name, helptext, [labelmap[j] for j in self.labels])
+        self.lock = threading.Lock()
+        self.old_model_keys = set()
+        self.name = name
         self.update()
 
     def update(self):
-        """Interrogate the model and update all labels to match."""
-        for new_model_dict in self.model().values():
-            self._update(new_model_dict)
+        """Interrogate the model and update all labels to match.
+
+        You must call this when the model changes, or the gauges won't update.
+
+        If a primary key goes away (that is, there was a value of idlabel
+        in the old model but there is not in the new model), we remove
+        its entry / label from the gauge.
+        """
+        model = self.model()
+        with self.lock:
+            for (k, new_model_dict) in model.items():
+                # if this assertion fails, the model is invalid. each key in a
+                # valid model should have a model dictionary mapping
+                # self.idlabel to the key
+                assert k == new_model_dict[self.idlabel], (k, new_model_dict)
+                self.old_model_keys.discard(k)
+                self._update(new_model_dict)
+            # now remove all keys that were in the old model but not the new
+            for k in self.old_model_keys:
+                old_labelvalues = self.id2labelvalues_map.get(k)
+                LOGGER.info(f'removing deleted primary key {old_labelvalues}')
+                assert old_labelvalues, (k, self.old_model_keys)
+                self.gauge.remove(*old_labelvalues)
+            self.old_model_keys = set(model.keys())
 
     def _update(self, new_kv):
         new_labelvalues = [new_kv.get(s, '') for s in self.labels]
@@ -109,6 +133,7 @@ class ModelGauge:
             self.gauge.labels(*new_labelvalues).set_function(_select)
             if old_labelvalues:
                 self.gauge.remove(*old_labelvalues)
+
 
 class UispClientGauge:
     def __init__(self, metric, metric_desc, copylabels, model, value_selector):
@@ -157,6 +182,7 @@ class PrometheusWrapper:
         self.id2allclients_map = {}
         self.id2client_map = {}
         self.id2service_map = {}
+        self.gauges = []
         self._maybe_refresh()
 
         clientlabels = [
@@ -171,17 +197,17 @@ class PrometheusWrapper:
             return self.id2client_map
         def _stateselector(model_dict):
             return ClientStatus.from_client(model_dict).value
-        self.clientstate_g = UispClientGauge('uisp_client_state', 'UISP client state', clientlabels, _clientmodel, _stateselector)
+        self.gauges.append(UispClientGauge('uisp_client_state', 'UISP client state', clientlabels, _clientmodel, _stateselector))
         contactlabels = ['id', 'clientId', 'email', 'phone', 'name', 'types']
         def _contactmodel():
             self._maybe_refresh()
             return { contact['id']: dict(contact, userIdent=client['userIdent'], types=','.join(sorted([t['name'] for t in contact['types']]))) for client in self.id2client_map.values() for contact in client['contacts'] }
-        self.clientcontact_g = UispClientGauge('uisp_client_contact', 'UISP client contact info', contactlabels, _contactmodel, lambda model_dict: 1)
-        self.clientbalance_g = UispClientGauge('uisp_client_balance', 'UISP client balance, negative means client owes us', ['id', 'currencyCode'], _clientmodel, lambda d: d['accountBalance'])
-        self.clientpastdue_g = UispClientGauge('uisp_client_pastdue', 'UISP client pastdue balance', ['id'], _clientmodel, lambda d: d['hasOverdueInvoice'])
-        self.clientautopay_g = UispClientGauge('uisp_client_autopay', 'UISP client has autopay enabled', ['id'], _clientmodel, lambda d: d['hasAutopayCreditCard'])
-        self.clientinvited_g = UispClientGauge('uisp_client_invited_ts', 'UISP client invitation timestamp', ['id'], _clientmodel, lambda d: d['invitationEmailSentDate'])
-        self.clientregistered_g = UispClientGauge('uisp_client_registered_ts', 'UISP client registration timestamp', ['id'], _clientmodel, lambda d: d['registrationDate'])
+        self.gauges.append(UispClientGauge('uisp_client_contact', 'UISP client contact info', contactlabels, _contactmodel, lambda model_dict: 1))
+        self.gauges.append(UispClientGauge('uisp_client_balance', 'UISP client balance, negative means client owes us', ['id', 'currencyCode'], _clientmodel, lambda d: d['accountBalance']))
+        self.gauges.append(UispClientGauge('uisp_client_pastdue', 'UISP client pastdue balance', ['id'], _clientmodel, lambda d: d['hasOverdueInvoice']))
+        self.gauges.append(UispClientGauge('uisp_client_autopay', 'UISP client has autopay enabled', ['id'], _clientmodel, lambda d: d['hasAutopayCreditCard']))
+        self.gauges.append(UispClientGauge('uisp_client_invited_ts', 'UISP client invitation timestamp', ['id'], _clientmodel, lambda d: d['invitationEmailSentDate']))
+        self.gauges.append(UispClientGauge('uisp_client_registered_ts', 'UISP client registration timestamp', ['id'], _clientmodel, lambda d: d['registrationDate']))
 
         servicelabels = [
             'id', 'clientId', 'prepaid', 'addressGpsLat', 'addressGpsLon',
@@ -191,11 +217,11 @@ class PrometheusWrapper:
         def _servicemodel():
             self._maybe_refresh()
             return self.id2service_map
-        self.servicestate_g = UispClientGauge('uisp_service_state', 'UISP service state', servicelabels, _servicemodel, lambda d: d['status'])
-        self.serviceactivefrom_g = UispClientGauge('uisp_service_active_from_ts', 'UISP service start timestamp', ['id', 'clientId'], _servicemodel, lambda d: d['activeFrom'])
-        self.serviceactiveto_g = UispClientGauge('uisp_service_active_to_ts', 'UISP service end timestamp, 0=ongoing', ['id', 'clientId'], _servicemodel, lambda d: d['activeTo'])
-        self.servicecontractend_g = UispClientGauge('uisp_service_contract_end_ts', 'UISP service contract end timestamp, 0=no contract', ['id', 'clientId'], _servicemodel, lambda d: d['contractEndDate'])
-        self.serviceinvoiced_g = UispClientGauge('uisp_service_last_invoiced_ts', 'UISP service contract last invoiced timestamp, 0=never invoiced', ['id', 'clientId'], _servicemodel, lambda d: d['lastInvoicedDate'])
+        self.gauges.append(UispClientGauge('uisp_service_state', 'UISP service state', servicelabels, _servicemodel, lambda d: d['status']))
+        self.gauges.append(UispClientGauge('uisp_service_active_from_ts', 'UISP service start timestamp', ['id', 'clientId'], _servicemodel, lambda d: d['activeFrom']))
+        self.gauges.append(UispClientGauge('uisp_service_active_to_ts', 'UISP service end timestamp, 0=ongoing', ['id', 'clientId'], _servicemodel, lambda d: d['activeTo']))
+        self.gauges.append(UispClientGauge('uisp_service_contract_end_ts', 'UISP service contract end timestamp, 0=no contract', ['id', 'clientId'], _servicemodel, lambda d: d['contractEndDate']))
+        self.gauges.append(UispClientGauge('uisp_service_last_invoiced_ts', 'UISP service contract last invoiced timestamp, 0=never invoiced', ['id', 'clientId'], _servicemodel, lambda d: d['lastInvoicedDate']))
 
         self.errors_g = prometheus_client.Gauge('uisp_errors', 'Number of errors')
         self.errors_g.set_function(lambda: self.errors)
@@ -206,6 +232,8 @@ class PrometheusWrapper:
                 try:
                     self._refresh()
                     self.last_update = time.time()
+                    for g in self.gauges:
+                        g.update()
                 except requests.exceptions.ReadTimeout:
                     LOGGER.exception()
                     self.errors += 1
